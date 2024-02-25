5097a81cc5831fe0265066b7e9cfeee0790ec8d0
Vegas-in <angeldevegas@gmail.com>
2024-02-25 12:43:13 +0100

    depuracion errores y creacion validacion respuestas

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
 # Quiz
 
+Proyecto indiviual para crear un juego de preguntas y respuestas múltiples aplicando los conocimientos de JavaScript aprendidos hasta el momento. Enviar la URL del repositorio en el que lo hayáis subido. 
+
+Objetivo
+El Quiz constará de 10 preguntas. Cada pregunta tendrá 4 opciones y sólo una de ellas será la correcta.
+Visualizar un único formulario con 10 preguntas en el navegador (en un mismo documento HTML)
+Utilizar un único <form>
+Cuando hagas "Enviar", se realizará la validación del formulario
+Requisitos
+Manipulación dinámica del DOM
+Sin frameworks ni librerias externas en la medida de lo posible
+Gestión del proyecto desde el inicio en un único repositorio en Github. Usar el sistema de ramas, main, develop, etc... como hemos visto en clase.
+Código limpio, buenas prácticas
+Documentar bien el README
+Subirlo GitHub Pages y compartid el enlace (y ponedlo en el fichero Readme).
+Referencia: https://github.com/TheBridge-FullStackDeveloper/proyectos-quiz
+
+
+ENLACE A GITHUB PAGES DEL QUIZ:
+
+vegas-in.github.io/Quiz/
+
+
+PREGUTNAS Y SOLUCIONES PARA EL QUIZ:
+
 ¿Quién es considerado el hombre que inventó el teléfono? Alexander Graham Bell
 Este planeta gira más rápido, completando una rotación completa en solo 10 horas. ¿Qué planeta es? Júpiter
 
--- script.js
+++ script.js
@@ -1,10 +1,20 @@
+const preguntas = ["¿Quién es considerado el hombre que inventó el teléfono?", "Este planeta gira más rápido, completando una rotación completa en solo 10 horas. ¿Qué planeta es?", "¿Cuál es la sustancia natural más dura de la Tierra?", "¿Cuántos dientes tiene un ser humano adulto?", "Este animal fue el primero en ser lanzado al espacio. La ataron a la nave espacial soviética Sputnik 2 que fue enviada al espacio exterior el 3 de noviembre de 1957. ¿Cuál era su nombre?", "¿Quién fue la primera mujer en el espacio?", "¿En qué parte del cuerpo humano se encuentran la mayoría de las glándulas sudoríparas?", "¿cuánto tarda la luz del sol en llegar a la Tierra?", "¿Cuántos huesos hay en el cuerpo humano?", "¿Cómo se llama el proceso de descomposición de los alimentos?"];
+
+const respuestas = ["Graham Bell", "Júpiter", "Diamante", "32", "Laika", "Valentina tereshkova", "Parte inferior de los pies", "8 minutos", "206", "Digestión"];
+
+const respuesFalsas = ["Newton", "Tesla", "Telefonica", "Marte", "Pandora", "Tatooine", "Hulk", "Titanio", "Adamantium", "23", "58", "1024", "Lassie", "Scooby-DO", "Snoopy", "Capitana Marvel", "Belén Esteban", "Marie Curie", "Las axilas", "Los ojos", "Las pestañas", "8 horas", "8 dias", "8 nanosegundos", "207", "307", "308", "Implosión", "Desintegración", "Hadouken"]
+
+
+
+function shuffle (array){                 // funcion copiada para solucionar para que mezcle las poscioones de las respuestas
+    array.sort(()=> Math.random() -0.5);
+}
+
+
+
 function pintarTablas() {  
 
-    const preguntas = ["¿Quién es considerado el hombre que inventó el teléfono?", "Este planeta gira más rápido, completando una rotación completa en solo 10 horas. ¿Qué planeta es?", "¿Cuál es la sustancia natural más dura de la Tierra?", "¿Cuántos dientes tiene un ser humano adulto?", "Este animal fue el primero en ser lanzado al espacio. La ataron a la nave espacial soviética Sputnik 2 que fue enviada al espacio exterior el 3 de noviembre de 1957. ¿Cuál era su nombre?", "¿Quién fue la primera mujer en el espacio?", "¿En qué parte del cuerpo humano se encuentran la mayoría de las glándulas sudoríparas?", "¿cuánto tarda la luz del sol en llegar a la Tierra?", "¿Cuántos huesos hay en el cuerpo humano?", "¿Cómo se llama el proceso de descomposición de los alimentos?"];
-
-    const respuestas = ["Graham Bell", "Júpiter", "Diamante", "32", "Laika", "Valentina tereshkova", "Parte inferior de los pies", "8 minutos", "206", "Digestión"];
-
-    const respuesFalsas = ["Newton", "Tesla", "Telefonica", "Marte", "Pandora", "Tatooine", "Hulk", "Titanio", "Adamantium", "23", "58", "1024", "Lassie", "Scooby-DO", "Snoopy", "Capitana Marvel", "Belén Esteban", "Marie Curie", "Las axilas", "Los ojos", "Las pestañas", "8 horas", "8 dias", "8 nanosegundos", "207", "307", "308", "Implosión", "Desintegración", "Hadouken"]
+    
 
     let contador = 0
 
@@ -17,40 +27,76 @@ function pintarTablas() {
         respuestasAleatorias.push(respuesFalsas[contador++])
         respuestasAleatorias.push(respuesFalsas[contador++])
         respuestasAleatorias.push(respuesFalsas[contador++])
-
+        shuffle(respuestasAleatorias)
 
         let template = `<table border="2px">
                                         <th colspan="2"><label>${preguntas[i]}</label></th>
                                         <tr>
                                             <td>
-                                                <input type="radio"/>
+                                                <input type="radio"  id="resp${i}"  value="${respuestasAleatorias[0]}"/>
                                                 <label>${respuestasAleatorias[0]}</label>
                                                 
                                             </td>
                                             <td>
-                                                <input type="radio"/>
+                                                <input type="radio" id="resp${i}" value="${respuestasAleatorias[1]}"/>
                                                 <label>${respuestasAleatorias[1]}</label>
                                                 
                                             </td>
                                         </tr>
                                         <tr>
                                             <td>
-                                                <input type="radio"/>
+                                                <input type="radio" id="resp${i}" value="${respuestasAleatorias[2]}"/>
                                                 <label>${respuestasAleatorias[2]}</label>
                                                 
                                             </td>
                                             <td>
-                                                <input type="radio"/>
+                                                <input type="radio" id="resp${i}" value="${respuestasAleatorias[3]}"/>
                                                 <label>${respuestasAleatorias[3]}</label>
-                                                
                                             </td>
                                         </tr>
                                 </table>`;
 
-                document.getElementById("contenedor").innerHTML += template;
+                document.getElementById("pregYRespu").innerHTML += template;
         
     }
-                
+    
 }
 
 pintarTablas();
+
+document.querySelector("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let respUsuario = [];
+
+    let msj = ""
+
+    respUsuario.push(event.target.resp0.value);
+    respUsuario.push(event.target.resp1.value);
+    respUsuario.push(event.target.resp2.value);
+    respUsuario.push(event.target.resp3.value);
+    respUsuario.push(event.target.resp4.value);
+    respUsuario.push(event.target.resp5.value);
+    respUsuario.push(event.target.resp6.value);
+    respUsuario.push(event.target.resp7.value);
+    respUsuario.push(event.target.resp8.value);
+    respUsuario.push(event.target.resp9.value);
+    
+    for (let i = 0; i < respUsuario.length; i++) {
+        if (respUsuario[i] == respuestas[i]) {
+        msj += `Has acertado, la respuesta correcta es ${respuestas[i]} \n`;
+       } 
+       else{
+        msj += `Has fallado, la respuesta correcta es ${respuestas[i]} \n`;
+       }
+        
+    }
+    alert(msj)
+    event.target.submit();
+});
+
+
+
+
+
+
